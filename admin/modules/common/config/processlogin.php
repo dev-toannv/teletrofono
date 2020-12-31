1e1917803b29e775834c5a004ea04183eb82dfb6
Vantoanr <ntoan2205@gmail.com>
2020-12-31 23:18:42 +0700

    first change require

---
+++ admin/modules/common/config/processlogin.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+	$error='';
+	if(isset($_SESSION['acc'])==true && isset($_SESSION['pass'])==true){
+		header("Location:index.php?module=interface&action=admin_infor");
+	}
+	else{
+		if(isset($_POST['sub'])){
+			$acc=$_POST['admin_acc'];
+			$pass=$_POST['admin_pass'];
+			$conn=mysqli_connect('localhost','root','','teletrofono');
+			if(!$conn){
+				die("Connect error : ".mysqli_connect_error());
+			}
+			else{
+				$sql="select * from superadmin where admin_acc='$acc' and admin_pass='$pass'";
+				$result=mysqli_query($conn,$sql);
+				$count=mysqli_num_rows($result);
+				if($count<=0){
+					$error="You are not an administrator";
+				}
+				else{	
+						$_SESSION['acc']=$acc;
+						$_SESSION['pass']=$pass;
+						header("Location:index.php?module=interface&action=admin_infor");
+				}
+			}
+		}
+	}
--- admin/modules/common/login.php
+++ admin/modules/common/login.php
@@ -1,35 +1,6 @@
-
-<?php
-	session_start();
-	$error='';
-	if(isset($_SESSION['acc'])==true && isset($_SESSION['pass'])==true){
-		header("Location:index.php?module=interface&action=admin_infor");
-	}
-	else{
-		if(isset($_POST['sub'])){
-			$acc=$_POST['admin_acc'];
-			$pass=$_POST['admin_pass'];
-			$conn=mysqli_connect('localhost','root','','teletrofono');
-			if(!$conn){
-				die("Connect error : ".mysqli_connect_error());
-			}
-			else{
-				$sql="select * from superadmin where admin_acc='$acc' and admin_pass='$pass'";
-				$result=mysqli_query($conn,$sql);
-				$count=mysqli_num_rows($result);
-				if($count<=0){
-					$error="Ban khong phai admin";
-				}
-				else{	
-						$_SESSION['acc']=$acc;
-						$_SESSION['pass']=$pass;
-						header("Location:index.php?module=interface&action=admin_infor");
-				}
-			}
-		}
-	}
+<?php 
+	require_once("config/processlogin.php");
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
