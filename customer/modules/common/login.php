e5a6eddc5aba30e09ef738c054d12e6cf9dbcaf0
Vantoanr <ntoan2205@gmail.com>
2021-01-01 17:40:19 +0700

    Add sign up

--- customer/modules/common/login.php
+++ customer/modules/common/login.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Error</title>
+	<title>Customer login</title>
 </head>
 <body>
-	<p>Loi</p>
+	
 </body>
 </html>
---
+++ customer/modules/sign_up/sign_up.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sign up</title>
+	<link rel="stylesheet" href="modules/sign_up/sign_up.css">
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<div class="ff" id="lg">
+				<a href="index.php?module=sign_up&action=sign_up">
+					<img src="modules/sign_up/img/logo3.png" id="logo"alt="">
+				</a>
+			</div>
+			<div class='ff' id="chu">
+				<p id="chu1">
+					Đăng ký tài khoản ISTORE
+				</p>
+			</div>
+		</div>
+		<div id="body">
+			<div id="bodyleft">
+				
+			</div>
+			<div id="bodyright">
+				
+			</div>
+		</div>
+		<div id="footer">
+			
+		</div>
+	</div>
+</body>
+</html>
