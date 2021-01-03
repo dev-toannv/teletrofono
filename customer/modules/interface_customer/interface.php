0e51284429867b21eeaa83d8de302915f4c47f7d
Vantoanr <ntoan2205@gmail.com>
2021-01-03 21:46:58 +0700

    change overflow

--- customer/modules/interface_customer/interface.php
+++ customer/modules/interface_customer/interface.php
@@ -11,6 +11,18 @@
 	<title>Document</title>
 	<link rel="stylesheet" type="text/css" href="modules/header/header.css">
 	<link rel="stylesheet" type="text/css" href="modules/body/body.css">
+	<style type="text/css">
+		#div1{
+			 width: 100px;
+			 height: 100px;
+			 background: purple;
+			 -webkit-transition: width 2s; /* Safari */
+			 transition: width 2s;
+			}
+		#div1:hover {
+		 width: 300px;
+		}
+	</style>
 </head>
 <body>
 	<?php 
@@ -19,5 +31,9 @@
 	<?php 
 		require_once("modules/body/body.php");
 	?>
+	<div id="div1">
+		
+	</div>
+
 </body>
 </html>
