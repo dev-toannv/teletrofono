7378224229b98222ff1b30401c1933c4c25dd9e5
Vantoanr <ntoan2205@gmail.com>
2021-01-22 02:38:07 +0700

    moi

--- admin/modules/common/config/processlogin.php
+++ admin/modules/common/config/processlogin.php
@@ -9,7 +9,8 @@
 			$acc=$_POST['admin_acc'];
 			$pass=$_POST['admin_pass'];
 			require_once("modules/config/connectdb.php");
-			else{
+			
+
 				$sql="select * from manager where manager_code='$acc' and manager_password='$pass' and user_type='1'";
 				$result=mysqli_query($conn,$sql);
 				$count=mysqli_num_rows($result);
@@ -21,6 +22,6 @@
 						$_SESSION['admin_pass']=$pass;
 						header("Location:index.php?module=interface&action=interfaceAdmin");
 				}
-			}
+
 		}
 	}
